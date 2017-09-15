3cf7bb58dd45c7bda64713d4b45d37231c4c8931
Alan <alan.boglioli@gmail.com>
2017-09-15 15:28:53 -0300

    Add services with fake prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,33 +11,82 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.fields = [
       {
-        title: 'Marketing',
+        title: 'Web',
         selected: false,
+        defaultItems: [0, 1, 2, 3],
         items: [
-          {title: 'Algo', value: 125.00, selected: false},
-          {title: 'Asd', value: 121.00, selected: false},
-          {title: 'Qwe asd ad qwe qasczxc', value: 130.00, selected: false},
-          {title: 'Zxc', value: 756.00, selected: false},
-          {title: 'Vasc', value: 120.00, selected: false},
-          {title: 'Qacaqeqe', value: 145.50, selected: false},
+          {title: 'UX/UI', value: 125.00, selected: false},
+          {title: 'SEO', value: 121.00, selected: false},
+          {title: 'Servidor y dominio', value: 130.00, selected: false},
+          {title: 'Landing page', value: 756.00, selected: false},
+          {title: 'Web personalizada', value: 120.00, selected: false},
+          {title: 'E-commerce', value: 145.50, selected: false},
         ]
       },
       {
-        title: 'Desarrollo',
+        title: 'Marketing',
         selected: false,
+        defaultItems: [0, 1, 2, 3, 4, 5],
         items: [
-          {title: 'Ago', value: 125.00, selected: false},
-          {title: 'Asd', value: 21.00, selected: false},
-          {title: 'Qe', value: 10.00, selected: false},
-          {title: 'Zc', value: 7560.00, selected: false},
-          {title: 'Vasc', value: 120.00, selected: false},
-          {title: 'Qasc', value: 145.50, selected: false},
-          {title: 'Ago', value: 125.00, selected: false},
-          {title: 'Asd', value: 21.00, selected: false},
-          {title: 'Qe', value: 10.00, selected: false},
-          {title: 'Zc', value: 7560.00, selected: false},
-          {title: 'Vasc', value: 120.00, selected: false},
-          {title: 'Qasc', value: 145.50, selected: false},
+          {title: 'Análsis de mercado', value: 125.00, selected: false},
+          {title: 'Análisis de competencia', value: 21.00, selected: false},
+          {title: 'Análisis de público objetivo', value: 10.00, selected: false},
+          {title: 'Segmentación y posicionamiento', value: 7560.00, selected: false},
+          {title: 'Definición de plan estratégico', value: 120.00, selected: false},
+          {title: 'Asesoría de Marketing Digital', value: 145.50, selected: false},
+          {title: 'Marketing en punto de venta', value: 125.00, selected: false}
+        ]
+      },
+      {
+        title: 'Diseño',
+        selected: false,
+        defaultItems: [0, 1, 2],
+        items: [
+          {title: 'Identidad Corporativa', value: 125.00, selected: false},
+          {title: 'Papelería comercial', value: 21.00, selected: false},
+          {title: 'Diseño de flyers', value: 10.00, selected: false},
+          {title: 'Diseño de punto de venta', value: 7560.00, selected: false}
+        ]
+      },
+      {
+        title: 'Redes Sociales',
+        selected: false,
+        defaultItems: [0, 1, 2, 3],
+        items: [
+          {title: 'Configuración de cuentas', value: 125.00, selected: false},
+          {title: 'Atención al cliente', value: 125.00, selected: false},
+          {title: 'Generación de base de datos', value: 125.00, selected: false},
+          {title: 'Gestión de Facebook', value: 21.00, selected: false},
+          {title: 'Gestión de Instagram', value: 10.00, selected: false},
+          {title: 'Gestión de Twitter', value: 7560.00, selected: false},
+          {title: 'Gestión de Youtube', value: 7560.00, selected: false},
+        ]
+      },
+      {
+        title: 'Desarrollo/Sistemas',
+        selected: false,
+        defaultItems: [0, 1, 2],
+        items: [
+          {title: 'Administración de servidor', value: 125.00, selected: false},
+          {title: 'Mantenimiento web', value: 125.00, selected: false},
+          {title: 'Instalación y configuración de sistemas', value: 125.00, selected: false},
+          {title: 'Sistema personalizado', value: 125.00, selected: false},
+          {title: 'Aplicación móvil', value: 125.00, selected: false},
+          {title: 'Aplicación de escritorio', value: 125.00, selected: false}
+        ]
+      },
+      {
+        title: 'Publicidad Online',
+        selected: false,
+        defaultItems: [0, 1, 2],
+        items: [
+          {title: 'Configuración de perfiles publicitarios', value: 125.00, selected: false},
+          {title: 'Planificación de anuncios', value: 125.00, selected: false},
+          {title: 'Ejecución en Facebook', value: 125.00, selected: false},
+          {title: 'Ejecución en Instagram', value: 125.00, selected: false},
+          {title: 'Ejecución en Twitter', value: 125.00, selected: false},
+          {title: 'Ejecución en Youtube', value: 125.00, selected: false},
+          {title: 'Ejecución en Google', value: 125.00, selected: false},
         ]
       }
     ];
